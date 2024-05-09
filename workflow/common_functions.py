ac34e8b3832e34085cf523368472527a5c44e62d
Tim Booth <tim.booth@ed.ac.uk>
2024-05-09 16:40:11 +0100

    Now works collecting more then one channel into each output. Not sure if 50 is the optimal number. Maybe 10?

--- workflow/common_functions.py
+++ workflow/common_functions.py
@@ -33,3 +33,32 @@ def get_common_prefix(list_of_filenames, extn=r"\..+", base_only=True):
 
     return common_prefix
 
+def pore_to_batch(pore_num, batch_size):
+    """Given a pore number, return the batch where it should be batched.
+       I'll use inclusive ranges starting at 1, which is not Pythonic but
+       generally more intuitive. So for batch_size of 50 we'll have "1-50",
+       "51-100", "101-150" etc.
+    """
+    pore_num = int(pore_num)
+    assert pore_num >= 1
+
+    batch_size = int(batch_size)
+    if batch_size == 1:
+        # Special case
+        return str(pore_num)
+    assert batch_size >= 2
+
+    # OK, here we go
+
+    batch_start = ( ((pore_num - 1) // batch_size) * batch_size ) + 1
+    batch_end = batch_start + ( batch_size - 1 )
+
+    return f"{batch_start}-{batch_end}"
+
+# Quick test
+assert pore_to_batch(23, 1) == "23"
+assert pore_to_batch(1, 50)  == "1-50"
+assert pore_to_batch(50, 50) == "1-50"
+assert pore_to_batch(51, 50) == "51-100"
+assert pore_to_batch(99, 27) == "82-108"
+
